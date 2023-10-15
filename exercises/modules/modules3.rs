d10106895ae7691d80fac324ec3ccbed3de343a8
heylulu1122 <146460596+heylulu1122@users.noreply.github.com>
2023-10-15 21:26:02 +0800

    Update modules3.rs

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -8,11 +8,9 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // TODO: Complete this use statement
-use crate::time::SystemTime;
-use crate::time::UNIX_EPOCH;
+use std::time::{SystemTime , UNIX_EPOCH};
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
